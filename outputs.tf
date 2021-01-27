17deb06d5f15f3ca809d61a9f3cdcbfd7ff4cb76
nehrman <nicolas.ehrman@gmail.com>
2021-01-27 09:20:02 +0100

    Upgrade to 0.12

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,30 @@
 output "resource_group_id" {
   description = "id of resource group"
-  value       = "${azurerm_resource_group.rg.id}"
+  value       = azurerm_resource_group.rg.id
 }
 
 output "resource_group_name" {
   description = "name of resource group"
-  value       = "${azurerm_resource_group.rg.name}"
+  value       = azurerm_resource_group.rg.name
 }
 
 output "virtual_network_name" {
   description = "name of virtual network created in resource group"
-  value       = "${azurerm_virtual_network.net.name}"
+  value       = azurerm_virtual_network.net.name
 }
 
 output "virtual_network_id" {
   description = "id of virtual network created in resource group"
-  value       = "${azurerm_virtual_network.net.id}"
+  value       = azurerm_virtual_network.net.id
 }
 
 output "virtual_network_subnets_name" {
   description = "name of subnets created inside the new vNet"
-  value       = "${azurerm_subnet.subnet.*.name}"
+  value       = azurerm_subnet.subnet.*.name
 }
 
 output "virtual_network_subnets_id" {
   description = "The ids of subnets created inside the new vNet"
-  value       = "${azurerm_subnet.subnet.*.id}"
+  value       = azurerm_subnet.subnet.*.id
 }
+
